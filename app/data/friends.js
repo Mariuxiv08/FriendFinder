1096ec023fc976de48fa9bb6bda5b9aaedcda365
Mariuxi Vasconez <mariuxivasconez@Mariuxis-MBP.home>
2018-04-30 21:20:23 -0400

    Almost finished project

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+  {
+    name: "SpongeBob",
+    photo: "http://spongebob.wikia.com/wiki/SpongeBob_SquarePants_copies",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Patrick",
+    photo: "http://templeofblues.com/2018/04/10/pictures-of-patrick-from-spongebob/excellent-pictures-of-patrick-from-spongebob-3795/",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Mr. Krabs",
+    photo: "https://caseyljones.deviantart.com/art/Sketchmission-Buff-Mr-Krabs-719713989",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Plankton",
+    photo: "http://kartunspongebob1.blogspot.com/2014/11/spongebob-vs-plankton_29.html",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Mrs Puff",
+    photo: "http://spongebob.wikia.com/wiki/File:SpongeBob_SquarePants_Mrs._Puff_Character_1.png",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Squidward",
+    photo: "http://knowyourmeme.com/memes/handsome-squidward-squidward-falling",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
